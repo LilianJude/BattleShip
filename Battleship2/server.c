3d81b0381bb129e342fe41bdecd6ed2a62f1e626
Jude Lilian <lilian.jude2@gmail.com>
2018-03-27 10:40:55 +0200

    echange de fichiers

--- Battleship2/server.c
+++ Battleship2/server.c
@@ -81,15 +81,15 @@ void get_clients(int lis_sockfd, int * cli_sockfd)
     while(num_conn < 2)
     {
         /* Listen for clients. */
-	    listen(lis_sockfd, 253 - player_count);
+	if((listen(lis_sockfd, 10 - player_count)) < 0)
+		perror("ERROR: listen");
 
         clilen = sizeof(cli_addr);
 	
-	    /* Accept the connection from the client. */
+	/* Accept the connection from the client. */
         cli_sockfd[num_conn] = accept(lis_sockfd, (struct sockaddr *) &cli_addr, &clilen);
     
         if (cli_sockfd[num_conn] < 0)
-            /* Horrible things have happened. */
             perror("ERROR accepting a connection from a client."); 
         
         /* Send the client it's ID. */
@@ -126,9 +126,9 @@ int get_player_move(int cli_sockfd)
 }
 
 /* Checks that a players move is valid. */
-int check_move(char board[][3], int move, int player_id)
+int check_move(char board[][10], int move, int player_id)
 {
-    if ((move == 9) || (board[move/3][move%3] == ' ')) { /* Move is valid. */
+    if ((move == 9) || (board[move/10][move%10] == ' ')) { /* Move is valid. */
         
         #ifdef DEBUG
         printf("[DEBUG] Player %d's move was valid.\n", player_id);
@@ -146,19 +146,36 @@ int check_move(char board[][3], int move, int player_id)
 }
 
 /* Updates the board with a new move. */
-void update_board(char board[][3], int move, int player_id)
+void update_board(char board[][10], int move, int player_id)
 {
-    board[move/3][move%3] = player_id ? 'X' : 'O';
+    board[move/10][move%10] = player_id ? 'X' : 'O';
 }
 
 /* Draws the game board to stdout. */
-void draw_board(char board[][3])
+void draw_board(char board[][10])
 {
-    printf(" %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
-    printf("-----------\n");
-    printf(" %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
-    printf("-----------\n");
-    printf(" %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
+    printf("   A | B | C | D | E | F | G | H | I | J | \n");
+    printf("   -------------------------------------------\n");
+    printf("1 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5], board[0][6], board[0][7], board[0][8], board[0][9]);
+    printf("   -------------------------------------------\n");
+    printf("2 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5], board[1][6], board[1][7], board[1][8], board[1][9]);
+    printf("   -------------------------------------------\n");
+    printf("3 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5], board[2][6], board[2][7], board[2][8], board[2][9]);
+    printf("   -------------------------------------------\n");
+    printf("4 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5], board[3][6], board[3][7], board[3][8], board[3][9]);
+    printf("   -------------------------------------------\n");
+    printf("5 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[4][0], board[4][1], board[4][2], board[4][3], board[4][4], board[4][5], board[4][6], board[4][7], board[4][8], board[4][9]);
+    printf("   -------------------------------------------\n");
+    printf("6 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[5][0], board[5][1], board[5][2], board[5][3], board[5][4], board[5][5], board[5][6], board[5][7], board[5][8], board[5][9]);
+    printf("   -------------------------------------------\n");
+    printf("7 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[6][0], board[6][1], board[6][2], board[6][3], board[6][4], board[6][5], board[6][6], board[6][7], board[6][8], board[6][9]);
+    printf("   -------------------------------------------\n");
+    printf("8 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[7][0], board[7][1], board[7][2], board[7][3], board[7][4], board[7][5], board[7][6], board[7][7], board[7][8], board[7][9]);
+    printf("   -------------------------------------------\n");
+    printf("9 | %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[8][0], board[8][1], board[8][2], board[8][3], board[8][4], board[8][5], board[8][6], board[8][7], board[8][8], board[8][9]);
+    printf("   -------------------------------------------\n");
+    printf("10| %c | %c | %c | %c | %c | %c | %c | %c | %c | %c | \n", board[9][0], board[9][1], board[9][2], board[9][3], board[9][4], board[9][5], board[9][6], board[9][7], board[9][8], board[9][9]);
+
 }
 
 /* Sends a board update to both clients. */
@@ -182,11 +199,11 @@ void send_player_count(int cli_sockfd)
 }
 
 /* Checks the board to determine if there is a winner. */
-int check_board(char board[][3], int last_move)
+int check_board(char board[][10], int last_move)
 {
    
-    int row = last_move/3;
-    int col = last_move%3;
+    int row = last_move/10;
+    int col = last_move%10;
 
     if ( board[row][0] == board[row][1] && board[row][1] == board[row][2] ) { /* Check the row for a win. */
 
@@ -219,10 +236,16 @@ int check_board(char board[][3], int last_move)
 void *run_game(void *thread_data) 
 {
     int *cli_sockfd = (int*)thread_data; /* Client sockets. */
-    char board[3][3] = { {' ', ' ', ' '}, /* Game Board */ 
-                         {' ', ' ', ' '}, 
-                         {' ', ' ', ' '} };
-
+    char board[10][10] = { {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, /* Game Board */ 
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+			   {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}, };
     printf("Game on!\n");
     
     /* Send the start message. */
