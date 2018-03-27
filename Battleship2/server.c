cd783fdfdf9f4647900843c7094d320274640733
Alexandre Guillon <cyclopedeveloppeur@gmail.com>
2018-03-27 10:35:47 -0400

    changement commentaires

--- Battleship2/server.c
+++ Battleship2/server.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -28,10 +28,10 @@ int recv_int(int cli_sockfd)
 {
     int msg = 0;
     int n = recv(cli_sockfd, &msg, sizeof(int), 0);
-    
+
     if (n < 0 || n != sizeof(int)) /* Not what we were expecting. Client likely disconnected. */
         return -1;
-    
+
     return msg;
 }
 
@@ -70,40 +70,39 @@ void send_clients_int(int * cli_sockfd, int msg)
 }
 
 
-/* Sets up the client sockets and client connections. */
+/* Cette fonction permet de se mettre en attente jusqu'à ce que deux clients se connectent sur le serveur de jeu pour ainsi créer une salle. */
 void get_clients(int lis_sockfd, int * cli_sockfd)
 {
     socklen_t clilen;
     struct sockaddr_in serv_addr, cli_addr;
 
-    /* Listen for two clients. */
+    /* On attend les deux clients */
     int num_conn = 0;
     while(num_conn < 2)
     {
-        /* Listen for clients. */
+        /* On écoute jusqu'à ce qu'un client se connecte */
 	    listen(lis_sockfd, 253 - player_count);
 
         clilen = sizeof(cli_addr);
-	
-	    /* Accept the connection from the client. */
+
+	    /* On accepte la connexion du client. */
         cli_sockfd[num_conn] = accept(lis_sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    
+
         if (cli_sockfd[num_conn] < 0)
-            /* Horrible things have happened. */
-            perror("ERROR accepting a connection from a client."); 
-        
-        /* Send the client it's ID. */
+            perror("ERROR accepting a connection from a client.");
+
+        /* On envoie au client son identifiant. */
         send(cli_sockfd[num_conn], &num_conn, sizeof(int), 0);
-        
-        /* Increment the player count. */
+
+        /* On utilise un mutex afin d'incrémenter le nombre de joueurs connectés. */
         pthread_mutex_lock(&mutexcount);
         player_count++;
-        printf("Number of players is now %d.\n", player_count);
+        printf("Il y a désormais %d joueurs connecté(s).\n", player_count);
         pthread_mutex_unlock(&mutexcount);
 
         if (num_conn == 0) {
             /* Send "HLD" to first client to let the user know the server is waiting on a second client. */
-            send_client_msg(cli_sockfd[0],"HLD"); 
+            send_client_msg(cli_sockfd[0],"HLD");
         }
 
         num_conn++;
@@ -117,7 +116,7 @@ void get_clients(int lis_sockfd, int * cli_sockfd)
 /* Gets a move from a client. */
 int get_player_move(int cli_sockfd)
 {
-    
+
     /* Tell player to make a move. */
     send_client_msg(cli_sockfd, "TRN");
 
@@ -129,18 +128,9 @@ int get_player_move(int cli_sockfd)
 int check_move(char board[][3], int move, int player_id)
 {
     if ((move == 9) || (board[move/3][move%3] == ' ')) { /* Move is valid. */
-        
-        #ifdef DEBUG
-        printf("[DEBUG] Player %d's move was valid.\n", player_id);
-        #endif
-        
         return 1;
    }
    else { /* Move is invalid. */
-       #ifdef DEBUG
-       printf("[DEBUG] Player %d's move was invalid.\n", player_id);
-       #endif
-    
        return 0;
    }
 }
@@ -164,12 +154,12 @@ void draw_board(char board[][3])
 /* Sends a board update to both clients. */
 void send_update(int * cli_sockfd, int move, int player_id)
 {
-    /* Signal an update */    
+    /* Signal an update */
     send_clients_msg(cli_sockfd, "UPD");
 
     /* Send the id of the player that made the move. */
     send_clients_int(cli_sockfd, player_id);
-    
+
     /* Send the move. */
     send_clients_int(cli_sockfd, move);
 }
@@ -184,7 +174,7 @@ void send_player_count(int cli_sockfd)
 /* Checks the board to determine if there is a winner. */
 int check_board(char board[][3], int last_move)
 {
-   
+
     int row = last_move/3;
     int col = last_move%3;
 
@@ -198,38 +188,34 @@ int check_board(char board[][3], int last_move)
     }
     else if (!(last_move % 2)) { /* If the last move was at an even numbered position we have to check the diagonal(s) as well. */
         if ( (last_move == 0 || last_move == 4 || last_move == 8) && (board[1][1] == board[0][0] && board[1][1] == board[2][2]) ) {  /* Check backslash diagonal. */
-            #ifdef DEBUG
-            printf("[DEBUG] Win by backslash diagonal.\n");
-            #endif 
+            printf("Win by backslash diagonal.\n");
             return 1;
         }
         if ( (last_move == 2 || last_move == 4 || last_move == 6) && (board[1][1] == board[0][2] && board[1][1] == board[2][0]) ) { /* Check frontslash diagonal. */
-            #ifdef DEBUG
-            printf("[DEBUG] Win by frontslash diagonal.\n");
-            #endif 
+            printf("Win by frontslash diagonal.\n");
             return 1;
         }
     }
-    
+
     /* No winner, yet. */
     return 0;
 }
 
 /* Runs a game between two clients. */
-void *run_game(void *thread_data) 
+void *run_game(void *thread_data)
 {
     int *cli_sockfd = (int*)thread_data; /* Client sockets. */
-    char board[3][3] = { {' ', ' ', ' '}, /* Game Board */ 
-                         {' ', ' ', ' '}, 
+    char board[3][3] = { {' ', ' ', ' '}, /* Game Board */
+                         {' ', ' ', ' '},
                          {' ', ' ', ' '} };
 
-    printf("Game on!\n");
-    
+    printf("Hello world sur fond rose\n");
+
     /* Send the start message. */
     send_clients_msg(cli_sockfd, "SRT");
 
     draw_board(board);
-    
+
     int prev_player_turn = 1;
     int player_turn = 0;
     int game_over = 0;
@@ -246,7 +232,7 @@ void *run_game(void *thread_data)
             if (move == -1) break; /* Error reading client socket. */
 
             printf("Player %d played position %d\n", player_turn, move);
-                
+
             valid = check_move(board, move, player_turn);
             if (!valid) { /* Move was invalid. */
                 printf("Move was invalid. Let's try this again...\n");
@@ -266,13 +252,13 @@ void *run_game(void *thread_data)
             /* Update the board and send the update. */
             update_board(board, move, player_turn);
             send_update( cli_sockfd, move, player_turn );
-                
+
             /* Re-draw the board. */
             draw_board(board);
 
             /* Check for a winner/loser. */
             game_over = check_board(board, move);
-            
+
             if (game_over == 1) { /* We have a winner. */
                 send_client_msg(cli_sockfd[player_turn], "WIN");
                 send_client_msg(cli_sockfd[(player_turn + 1) % 2], "LSE");
@@ -303,69 +289,62 @@ void *run_game(void *thread_data)
     player_count--;
     printf("Number of players is now %d.", player_count);
     pthread_mutex_unlock(&mutexcount);
-    
+
     free(cli_sockfd);
 
     pthread_exit(NULL);
 }
 
-/* 
+/*
  * Main Program
  */
 
 int main(int argc, char *argv[])
-{   
+{
 
     int sockfd;
     struct sockaddr_in serv_addr;
 
-    /* Get a socket to listen on */
+    /* Donne une socket à écouter */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         perror("ERROR opening listener socket.");
-    
-    /* set up the server info */
-    serv_addr.sin_family = AF_INET;	
-    serv_addr.sin_addr.s_addr = INADDR_ANY;	
-    serv_addr.sin_port = htons(MYPORT);		
 
-    /* Bind the server info to the listener socket. */
+    /* Etablissement des informations du serveur */
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(MYPORT);
+
+    /* Donne les informations du serveur à la socket d'écoute. */
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         perror("ERROR binding listener socket.");
 
-    int lis_sockfd = sockfd; /* Listener socket. */
+    int lis_sockfd = sockfd; /* Socket d'écoute. */
 
     pthread_mutex_init(&mutexcount, NULL);
 
     while (1) {
-        if (player_count <= 252) { /* Only launch a new game if we have room. Otherwise, just spin. */  
-            int *cli_sockfd = (int*)malloc(2*sizeof(int)); /* Client sockets */
-            
-            /* Get two clients connected. */
+        if (player_count <= 252) { /* On lance une nouvelle partie seulement s'il y a des salles disponibles (c'est à dire moins de 252 joueurs en même temps). */
+            int *cli_sockfd = (int*)malloc(2*sizeof(int)); /* Sockets des clients */
+
+            /* On attend deux clients. */
             get_clients(lis_sockfd, cli_sockfd);
-            
-            #ifdef DEBUG
-            printf("[DEBUG] Starting new game thread...\n");
-            #endif
 
             pthread_t thread;
 
-	    /* Start a new thread for this game. */
-            int result = pthread_create(&thread, NULL, run_game, (void *)cli_sockfd); 
+	    /* On lance une nouvelle partie. */
+            int result = pthread_create(&thread, NULL, run_game, (void *)cli_sockfd);
 
             if (result){
                 printf("Thread creation failed with return code %d\n", result);
                 exit(-1);
             }
-            
-            #ifdef DEBUG
-            printf("[DEBUG] New game thread started.\n");
-            #endif
+
         }
     }
 
     close(lis_sockfd);
 
     pthread_mutex_destroy(&mutexcount);
-    pthread_exit(NULL); 
+    pthread_exit(NULL);
 }
